e6e5898e532ac914e3a2de2e992f52c27d1f53e5
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-12 12:09:59 +0100

    Update and rename resultset4a.js to resultset4b.js

--- resultset4b.js
+++ resultset4b.js
@@ -39,7 +39,7 @@ var getScriptPromisify = (src) => {
         <div id="my_data">data...</div>
       </div>
     `
-  class myResultSet4a extends HTMLElement {
+  class myResultSet4b extends HTMLElement {
     constructor () {
       super()
 
@@ -56,27 +56,14 @@ var getScriptPromisify = (src) => {
     // ------------------
     async render (resultSet) {
       // await getScriptPromisify('https://cdn.bootcdn.net/ajax/libs/echarts/5.0.0/echarts.min.js')
-      console.log('resultSet:')
-      console.log(resultSet)
+      // console.log('resultSet:')
+      // console.log(resultSet)
       
-      console.log('now:')
-      // var my_data = document.getElementById("my_data")
-      // my_data.innerHtml='<h1>Hello member</h1>'
-      // document.getElementById("my_data").innerHTML = table_output
-      
-      // this._armando2 = this._shadowRoot.getElementById('my_data').innerHTML = "ole"
-      
-
       this._placeholder = this._root.querySelector('#placeholder')
       if (this._placeholder) {
         this._root.removeChild(this._placeholder)
         this._placeholder = null
       }
-      
-      //var MEASURE_DIMENSION = '@MeasureDimension'
-      //var countries = []
-      //var timeline = []
-      //var series = []
       
       // Table Headers definition
       var table_output = '<table><tr><th>Country</th><th>Year</th><th>Population</th><th>LifeExpect</th><th>Income</th></tr>'
@@ -88,25 +75,17 @@ var getScriptPromisify = (src) => {
          console.log(dp)
          var cCountry = dp.Country.description
          var ctimeline = dp.timeline.description
-
+         var { formattedValue, description } = dp[MeasureDimension]
+         
         if (counterRows === 1)
         {
-          var cPopulation = dp.Population.formattedValue
           table_output += '<tr><td>'+ cCountry +'</td>'
           table_output += '<td>'+ ctimeline +'</td>'
-          table_output += '<td>'+ cPopulation +'</td>'
-        } else if (counterRows === 2)
-        {
-           var cLifeExpect = dp.LifeExpect.formattedValue
+          table_output += '<td>'+ formattedValue +'</td>'
+        } else {
            table_output += '<td>'+ cCountry +'</td>'
            table_output += '<td>'+ ctimeline +'</td>'
-           table_output += '<td>'+ cLifeExpect +'</td>'
-        } else if (counterRows === 3)
-        {
-           var cIncome = dp.Income.formattedValue
-           table_output += '<td>'+ cCountry +'</td>'
-           table_output += '<td>'+ ctimeline +'</td>'
-           table_output += '<td>'+ cIncome +'</td>'
+           table_output += '<td>'+ formattedValue +'</td>'
         }
         
         counterRows = counterRows + 1
@@ -127,25 +106,14 @@ var getScriptPromisify = (src) => {
       
       console.log(table_output)
       
-      // Giving error: Cannot set properties of null (setting 'innerHTML')
-      // below line
-      //document.getElementById("my_data").innerHTML = table_output
-      
       // Works well below line
       //this._armando2 = this._shadowRoot.getElementById('my_data').innerHTML = table_output
       // this._shadowRoot.getElementById('my_data').innerHTML = table_output
-      
-       //var table_output2 = '<table><tr><th>Company</th><th>Contact</th><th>Country</th></tr><tr><td>Alfreds Futterkiste</td>'
-       //table_output2 += '<td>Maria Anders</td><td>Germany</td></tr><tr><td>Centro comercial Moctezuma</td><td>Francisco Chang</td>'
-       //table_output2 += '<td>Mexico</td></tr><tr><td>Ernst Handel</td><td>Roland Mendel</td><td>Austria</td></tr>'
-       //table_output2 += '<tr><td>Island Trading</td><td>Helen Bennett</td><td>UK</td></tr><tr><td>Laughing Bacchus Winecellars</td>'
-       //table_output2 += '<td>Yoshi Tannamuri</td><td>Canada</td></tr><tr><td>Magazzini Alimentari Riuniti</td><td>Giovanni Rovelli</td>'
-       //table_output2 += '<td>Italy</td></tr></table>'
       
       this._shadowRoot.getElementById('my_data').innerHTML = table_output
       
     }
   }
 
-  customElements.define('com-sap-sample-resultset4a', myResultSet4a)
+  customElements.define('com-sap-sample-resultset4b', myResultSet4b)
 })()
