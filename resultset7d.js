6a7fd534aa02c5036c941c3e33ea8c9e518aaec8
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-08-29 16:12:15 +0100

    Update and rename resultset7c.js to resultset7d.js

--- resultset7d.js
+++ resultset7d.js
@@ -7,6 +7,14 @@ var getScriptPromisify = (src) => {
 (function () {
   const template = document.createElement('template')
   template.innerHTML = `
+      <head>
+        <script language="javascript" type="text/javascript">
+            function myFunction() {
+                alert('Armando Jorge Araujo dos Santos - Prodigygnition')
+            }
+        </script>
+      </head>
+      
       <style>
       #root {
         background-color: white;
@@ -77,12 +85,6 @@ var getScriptPromisify = (src) => {
       
       </style>
       
-      <script language="javascript" type="text/javascript">
-          function myFunction() {
-              alert('Armando Jorge Araujo dos Santos - Prodigygnition')
-          }
-      </script>
-        
       <div id="root" style="width: 100%; height: 100%;">
 
         <input type="button" onclick="myFunction()" value="Hello World">
@@ -92,7 +94,7 @@ var getScriptPromisify = (src) => {
       </div>
     ` // Ending HTML code tag
   
-  class myResultSet7c extends HTMLElement {
+  class myResultSet7d extends HTMLElement {
     constructor () {
       super()
 
@@ -167,5 +169,5 @@ var getScriptPromisify = (src) => {
     }
   }
 
-  customElements.define('com-sap-sample-resultset7c', myResultSet7c)
+  customElements.define('com-sap-sample-resultset7d', myResultSet7d)
 })()
