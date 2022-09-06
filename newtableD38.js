c90adf64359c973132d153aa74d3b3a96b7fb416
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2022-09-06 14:57:35 +0100

    Update and rename newtableD37.js to newtableD38.js

--- newtableD38.js
+++ newtableD38.js
@@ -98,7 +98,7 @@ var getScriptPromisify = (src) => {
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   // HTML extension with all necessary logic(s) wrtitten JS vvvvvvvvvvvv
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv  
-  class myNewBlendTableD37 extends HTMLElement {
+  class myNewBlendTableD38 extends HTMLElement {
     constructor () {
       super()
 
@@ -330,7 +330,7 @@ var getScriptPromisify = (src) => {
                 //console.log("INC=" + IncomePercentage)
                 console.log("number_of_measures=" + number_of_measures)  
                 console.log("cCountry=" + cCountry)
-                console.log("ctimeline="+ctimeline)  
+                console.log("ctimeline=" + ctimeline)  
               
                 if (number_of_measures === 0) // Adding both variation %'s in one go
                 {
@@ -375,6 +375,6 @@ var getScriptPromisify = (src) => {
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
   // Return the end result to SAC (SAP ANALYTICS CLOUD) application vvvvvvvvvvvvvvvvvvvvv
   // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
-  customElements.define('com-sap-sample-newtabled37', myNewBlendTableD37)
+  customElements.define('com-sap-sample-newtabled38', myNewBlendTableD38)
   
 })() // END of function --> (function () {
