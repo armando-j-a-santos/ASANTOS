0a9d5adecee77a4720a5bb0631f7ec63b32331db
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-04-24 10:18:30 +0100

    co

--- sapuitable/asantos5.js
+++ sapuitable/asantos5.js
@@ -330,6 +330,9 @@
             var foundIndex = Ar.findIndex(x => x.id == widgetName);
             var divfinal = Ar[foundIndex].div;
             console.log(divfinal);
+            
+            console.log("oModel:");
+            console.log(oModel);
 
             
             //### THE APP: place the XMLView somewhere into DOM ###
