ca4be7d13c03ccd20ae498aaf673a05a857d34f3
Armando Santos <47860995+armando-j-a-santos@users.noreply.github.com>
2023-11-17 16:43:35 +0000

    v1.1

--- sapuitable/tableHier1.js
+++ sapuitable/tableHier1.js
@@ -90,6 +90,20 @@
         div3.innerHTML = '<div style="max-height: "' + that.max_height + that.unit_option + '"; border-radius: 15px; overflow-y: hidden;" id="ui5_content_' + widgetName + '" name="ui5_content_' + widgetName + '"><div style="max-height: ' + that.max_height + that.unit_option + '; border-radius: 15px; overflow-y: auto;" id="ui5_content_' + widgetName + '" name="ui5_content_' + widgetName + '"><slot name="content_' + widgetName + '"> </slot></div></div>';
          _shadowRoot.appendChild(div3);
 
+        
+        let div4 =document.createElement('div');
+        div4.innerHTML = `
+            <script src="https://openui5.hana.ondemand.com/resources/sap-ui-core.js"
+                type="text/javascript"
+                id="sap-ui-bootstrap"
+                data-sap-ui-libs="sap.m,sap.ui.commons,sap.ui.table,sap.ui.layout"
+                data-sap-ui-theme="sap_bluecrystal">
+            </script>
+        
+            <div id="dataTable"></div>  
+        `;
+
+        
         that_.appendChild(div);
         
 
@@ -147,61 +161,85 @@
                               }]
                             }];
                             */
+                           //ADD -------------------------------------------
+                            var oData = {  "catalog": {
+                                "clothing": {
+                                  "categories": [
+                                    {"name1": "Women", "amount": 160.99, "currency": "EUR", "size": "S", "selected": "X", "categories": [
+                                      {"name1":"Clothing", "categories": [
+                                         {"name1": "Dresses", "amount": 16.99, "currency": "EUR", "size": "S", "selected": "X", 
+                                          "categories": [
+                                         {"name1": "Casual Red Dress", "amount": 16.99, "currency": "EUR", "size": "S", "selected": "X"},
+                                         {"name1": "Short Black Dress", "amount": 47.99, "currency": "EUR", "size": "M"},
+                                         {"name1": "Long Blue Dinner Dress", "amount": 103.99, "currency": "USD", "size": "L"}
+                                        ]},
+                                        {"name1": "Tops", "categories": [
+                                          {"name1": "Printed Shirt", "amount": 24.99, "currency": "USD", "size": "M"},
+                                          {"name1": "Tank Top", "amount": 14.99, "currency": "USD", "size": "S"}
+                                        ]},
+                                        {"name1": "Pants", "categories": [
+                                          {"name1": "Red Pant", "amount": 32.99, "currency": "USD", "size": "M"},
+                                          {"name1": "Skinny Jeans", "amount": 44.99, "currency": "USD", "size": "S"},
+                                          {"name1": "Black Jeans", "amount": 99.99, "currency": "USD", "size": "XS"},
+                                          {"name1": "Relaxed Fit Jeans", "amount": 56.99, "currency": "USD", "size": "L"}
+                                        ]},
+                                        {"name1": "Skirts", "categories": [
+                                          {"name1": "Striped Skirt", "amount": 24.99, "currency": "USD", "size": "M"},
+                                          {"name1": "Black Skirt", "amount": 44.99, "currency": "USD", "size": "S"}
+                                        ]}
+                                      ]},
+                                      {"name1":"Jewelry", "categories": [
+                                          {"name1": "Necklace", "amount": 16.99, "currency": "USD"},
+                                          {"name1": "Bracelet", "amount": 47.99, "currency": "USD"},
+                                          {"name1": "Gold Ring", "amount": 399.99, "currency": "USD"}
+                                        ]},
+                                      {"name1":"Handbags", "categories": [
+                                        {"name1": "Little Black Bag", "amount": 16.99, "currency": "USD", "size": "S"},
+                                        {"name1": "Grey Shopper", "amount": 47.99, "currency": "USD", "size": "M"}
+                                      ]},
+                                      {"name1":"Shoes", "categories": [
+                                        {"name1": "Pumps", "amount": 89.99, "currency": "USD"},
+                                        {"name1": "Sport Shoes", "amount": 47.99, "currency": "USD"},
+                                        {"name1": "Boots", "amount": 103.99, "currency": "USD"}
+                                      ]}
+                                    ]}]}
+                            }};
+                            
+                            
+                             //ADD -------------------------------------------
+                            var treeTable = new sap.ui.table.TreeTable({
+                                title:"Tree Table",
+                                selectionMode : sap.ui.table.SelectionMode.None,
+                                editable:true,
+                                ariaLabelledBy:"title",
+                                toggleOpenState: function (oEvent) {}
+                            });
+                            
 
-                            var oData = [
-                                {
-                                  "text": "Node1",
-                                  "ref": "sap-icon://attachment-audio",
-                                  "nodes":
-                                  [
-                                      {
-                                          "text": "Node1-1",
-                                          "ref": "sap-icon://attachment-e-pub",
-                                          "nodes":[
-                                              {
-                                                  "text": "Node1-1-1",
-                                                  "ref": "sap-icon://attachment-html"
-                                              },
-                                              {
-                                                  "text": "Node1-1-2",
-                                                  "ref": "sap-icon://attachment-photo",
-                                                  "nodes":[
-                                                      {
-                                                          "text": "Node1-1-2-1",
-                                                          "ref": "sap-icon://attachment-text-file",
-                                                          "nodes":[
-                                                              {
-                                                                  "text": "Node1-1-2-1-1",
-                                                                  "ref": "sap-icon://attachment-video"
-                                                              },
-                                                              {
-                                                                  "text": "Node1-1-2-1-2",
-                                                                  "ref": "sap-icon://attachment-zip-file"
-                                                              },
-                                                              {
-                                                                  "text": "Node1-1-2-1-3",
-                                                                  "ref": "sap-icon://course-program"
-                                                              }
-                                                          ]
-                                                      }
-                                                  ]
-                                              }
-                                          ]
-                                      },
-                                      {
-                                          "text": "Node1-2",
-                                          "ref": "sap-icon://create"
-                                      }
-                                  ]
-                              },
-                              {
-                                  "text": "Node2",
-                                  "ref": "sap-icon://customer-financial-fact-sheet"
-                              }
-                            ];
+                            //ADD----------------   // Create the model linked to the data (oData)
+                            var _oModel = new sap.ui.model.json.JSONModel(OData);
+                            _oModel =   treeTable.setModel(OData);
+                            _oModel =   treeTable.addColumn(new sap.ui.table.Column({
+                                        label: new sap.ui.commons.Label({text: "Categories - AAA"}),
+                                        template: new sap.m.Text({ text : "{name1}"}),
+                                        sortProperty: "NscItem"}));
+                            _oModel =    treeTable.addColumn(new sap.ui.table.Column({
+                                        label: new sap.ui.commons.Label({text: "Armando"}),
+                                        template: new sap.m.Text({ text : "{amount}"}),
+                                        sortProperty: "NscItem"}));
+                            _oModel =   treeTable.addColumn(new sap.ui.table.Column({
+                                        label: new sap.ui.commons.Label({text: "SANTOS"}),
+                                        template: new sap.m.Text({ text : "{currency}"}),
+                                        sortProperty: "NscItem"}));
+                            _oModel =treeTable.addColumn(new sap.ui.table.Column({
+                                        label: new sap.ui.commons.Label({text: "Size"}),
+                                        template: new sap.m.Text({ text : "{size}"}),
+                                        sortProperty: "NscItem"}));
+                            _oModel =treeTable.bindRows("/catalog/clothing/");
+                            _oModel =treeTable.placeAt("dataTable");
 
                              // Create the model linked to the data (oData)
-                            var _oModel = new JSONModel(oData);
+                            //var _oModel = new JSONModel(oData);
                             _oModel.setSizeLimit(1000000);
                             
                             // Link the model to the widget
